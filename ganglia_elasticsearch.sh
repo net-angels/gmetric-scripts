c4563d8d853ecfe86c19a35be48b12b54e5ffa3c
Ara Sadoyan <ara.sadoyan@netangels.net>
2014-10-01 17:24:53 +0400

    update of ElastcSearch Monitor

--- ganglia_elasticsearch.sh
+++ ganglia_elasticsearch.sh
@@ -36,7 +36,7 @@ URL="http://127.0.0.1:9200/_nodes/_local/stats/?all=true"
 curl -s  "$URL" | sed -e 's/[{}]/''/g' \
         | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' \
         | grep '"query_total"\|"index_total"\|"get"\|heap_committed_in_bytes\|heap_used_in_bytes\|open_file_descriptors\|current_open' \
-        | sed -s s/'":"'/_/g| tr -d '"' | sed -s s/total/per_second/g | sed -s s/indexing_//g \
+        | sed -s s/'":"'/_/g| tr -d '"' | sed -s s/total/per_second/g | sed -s s/indexing_//g | sed -s s/mem_heap_used_in_bytes/heap_used_in_bytes/g \
         | tr ':' ' ' | awk '{print "es_"$1,$2}'> $Newdatafile
 
 es_metrics () {
@@ -45,7 +45,7 @@ es_metrics () {
         record_value_rate es_query_per_second
 	 
 	record_value es_open_file_descriptors
-	record_value es_mem_heap_used_in_bytes
+	record_value es_heap_used_in_bytes
 	record_value es_heap_committed_in_bytes
 	record_value es_non_heap_used_in_bytes
 	record_value es_non_heap_committed_in_bytes
